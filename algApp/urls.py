fcf84fa4af081f74d1d9005823b03e3e17c5c315
NakasatoH <nakasatoe50m@gmail.com>
2016-11-04 12:22:35 +0900

    画像を動かすことに成功

--- algApp/urls.py
+++ algApp/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^subanime/', views.subanime, name='subanime'),
-    url(r'^imagemove/', views.subanime, name='imagemove'),
+    url(r'^imagemove/', views.imagemove, name='imagemove'),
 ]
