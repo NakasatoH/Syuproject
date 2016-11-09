ca4c7621490e1509b458b210ea6bfbcdf4fa0ffa
NakasatoH <nakasatoe50m@gmail.com>
2016-11-09 10:05:33 +0900

    moveSample作成中(1)

--- static/js/moveSample.js
+++ static/js/moveSample.js
@@ -4,14 +4,25 @@
 // 画像オブジェクト生成
 var lemImage = new Image();
 
+var i = 0;
 // 画像パスを画像obj.srcに設定
-lemImage.src = pngurl;
+lemImage.src = lemSrc;
 
 // 画像の初回ロード時に画像を表示する
 lemImage.onload = (function () {   
     ImageToCanvas(lemImage);
 });
 
-function ImageToCanvas(im) {
-        
+function ImageToCanvas(im,) {
+    var canvas = document.getElementById('cvs');
+    var ctx = canvas.getContext('2d');
+
+    for(var j = 0;j < 10; j ++){
+        i += j;
+        // キャンバスをクリア
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.translate(i,i);
+        ctx.drawImage(im, 0, 0);
+        ctx.translate(-1 * i,-1 * i);
+    }
 }
