a6b49ff9a41db5726374ddf2ce927154ac228f2f
NakasatoH <nakasatoe50m@gmail.com>
2016-11-15 14:01:30 +0900

    キャラクター切り替え、一斉動作を実装

--- static/js/ddPlusMove.js
+++ static/js/ddPlusMove.js
@@ -4,6 +4,37 @@
 var old_dx = 0; // 今までの位置を保持しておくための変数old_...
 var old_dy = 0;
 var images = [];// ドロップした順にidを保管するための配列
+const moveNum = 100;
+
+// 画像オブジェクト生成
+var image1 = new Image();
+
+// 画像パスを画像obj.srcに設定
+image1.src = mainImageSrc;
+
+// 画像の初回ロード時に画像を表示する
+image1.onload = (function () {
+    ImageToCanvas(image1);
+});
+
+
+function changeCharacter() {
+    var charElm = document.getElementById("characters");
+    switch (charElm.selectedIndex){
+        case 0:
+            break;
+        case 1:
+            this.image1.src = lemSrc;
+            break;
+        case 2:
+            this.image1.src = t26e5Src;
+            break;
+        case 3:
+            this.image1.src = masaruSrc;
+            break;
+    }
+    ImageToCanvas(this.image1);
+}
 
 /**---------------------------------------------------------------------
  * D＆D関係
@@ -12,7 +43,6 @@ var images = [];// ドロップした順にidを保管するための配列
 // ドラッグ開始処理
 function f_dragstart(event) {
     event.dataTransfer.setData("text", event.target.id);// ドラッグするデータのid名をDataTransferオブジェクトにセット
-    console.log(event.dataTransfer.getData("text"));
 }
 
 // ドラッグ要素がドロップ要素に重なっている間の処理
@@ -35,25 +65,30 @@ function f_drop(event) {
      * その対策として、変数に値を代入して保持して億必要がある。
      */
     var currentTarget = event.currentTarget;
+    // コード画像をドロップしたとき不自然な挙動にならないよう遅延を作る
     window.setTimeout(function () {
         if (currentTarget.id == "bottom") {
             inputArray(id_name);// 全ての画像をもとのボックスに戻すため配列の中に入れる処理を追加
 
-            console.log("画像ファイル : " + lemImage.src + ", 方向 : " + data_d + ", 数値 : " + data_n);
+            console.log("画像ファイル : " + image1.src + ", 方向 : " + data_d + ", 数値 : " + data_n);
             console.log("ドロップ先     タグ名 : " + currentTarget.tagName + ", ID名 : " + currentTarget.id);
             console.log("追加する要素   タグ名 : " + drag_elm.tagName + ", ID名 : " + drag_elm.id);
 
             /**
              * キャンバス内の画像を動かす処理
              */
+
             var cnt = 0;// 無限ループを防ぐための変数cnt
             var imageMoveInterval = setInterval(function () {// setIntervalは引数を与える場合, 無名関数  →　function(){関数名(引数1,引数2,...)}　を使用しなければならない
                 console.log(imageMoveInterval);
-                if (cnt >= 100) {// if文を使用しないとループし続ける
+                if (cnt >= moveNum) {// if文を使用しないとループし続ける
                     clearInterval(imageMoveInterval);
                 }
                 cnt++;
-                ImageToCanvas(lemImage, data_d, data_n)
+                //----------------------------------------------------------------------------------
+                //ImageToCanvas(image1, data_d, data_n)
+                //----------------------------------------------------------------------------------
+
             }, 10);
         } else if (currentTarget.id == "upper") {
 
@@ -62,7 +97,7 @@ function f_drop(event) {
         imagesLog();
         currentTarget.appendChild(drag_elm);// ドロップ先にドラッグされた要素を追加をする
         event.preventDefault();// エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
-    }, 150);
+    }, 50);
 }
 
 /**------------------------------------------------------
@@ -103,21 +138,12 @@ function imagesLog() {
         console.log("images配列[" + i + "] : " + images[i]);
     }
 }
+
 /**------------------------------------------------------
  * 画像処理関係
  * ------------------------------------------------------
  */
-// 画像オブジェクト生成
-var lemImage = new Image();
-var i = 0;
 
-// 画像パスを画像obj.srcに設定
-lemImage.src = lemSrc;
-
-// 画像の初回ロード時に画像を表示する
-lemImage.onload = (function () {
-    ImageToCanvas(lemImage);
-});
 
 // canvas内の画像を動かす関数
 function ImageToCanvas(im, direction, num) {
@@ -174,4 +200,42 @@ function resetImages() {
     // 配列の長さを0にすることで配列を初期化（全要素を削除）
     images.length = 0;
     console.log("ドロップされた画像、配列を初期化")
+}
+
+/**--------------------------------------------------------
+ * ドロップされている画像群に従い、一斉に動作
+ * --------------------------------------------------------
+ */
+function action() {
+    var id;// ドロップされた画像のidを格納する変数
+    var data_d;// 方向
+    var data_n;// 移動量
+    var drop_elm;// ドロップ済みエレメント
+    var cnt = 0;// 無限ループを防ぐための変数cnt
+    var i = 0;
+
+    action();
+    // 全ての画像を順番に動かす。
+    function action() {
+        // ドロップされている画像群の個数と内容を把握する
+        id = images[i];
+        drop_elm = document.getElementById(id);
+        data_d = drop_elm.getAttribute("data-d");// 方向データ
+        data_n = drop_elm.getAttribute("data-n");// 移動量
+        console.log("dropElm,dataD,dataN :::" + drop_elm.id + " , " + data_d + " , " + data_n);
+        cnt = 0;
+        var itc = setInterval(function () {
+            if (cnt < moveNum) {
+                ImageToCanvas(image1, data_d, data_n);
+            } else {
+                i++;
+                if (i < images.length) {
+                    action();
+                }
+                clearInterval(itc);
+            }
+            cnt++;
+        }, 10);
+    }
+
 }
