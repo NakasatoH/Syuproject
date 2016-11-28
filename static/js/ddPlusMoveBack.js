4fddc1ef6f565e69d3a9e86ffbde958453fb3277
NakasatoH <nakasatoe50m@gmail.com>
2016-11-28 11:41:09 +0900

    2016/11/28 while処理作り直し時点

--- static/js/ddPlusMoveBack.js
+++ static/js/ddPlusMoveBack.js
@@ -15,20 +15,7 @@ image2.onload = (function () {
     ITC2(image2);
 });
 
-var map2 = [
-    ["*","*","*","*","*","*","*","*","*","*","*","*","*"],// (0,13)まで
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","0","0","0","0","0","0","0","0","0","0","0","*"],
-    ["*","*","*","*","*","*","*","*","*","*","*","*","*"]// (,13)まで
-];
 var map = mapData;
-console.log(map);
-console.log(map2);
 function arrayConverter() {
     
 }
@@ -41,19 +28,7 @@ function ITC2(im) {
     var canvas2 = document.getElementById('backCvs');
     var ctx2 = canvas2.getContext('2d');
     ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-    /*
-    for (var i = 0; dy < canvas2.height; i++) {
-        ctx2.translate(dx, dy);// 座標指定　初期(0,0)から
-        console.log("(dx , dy ) = (" + dx + "," + dy + ") imageSize - width :" + (dx + image2.width) + " height : " + (dy + image2.height));
-        ctx2.drawImage(im, 0, 0);// 画像貼り付け
-        ctx2.translate(-1 * dx, -1 * dy);
-        dx = dx + blockWidth;
-        if(dx > canvas2.width){
-            dx = 0;
-            dy += blockHeight;
-        }
-    }
-    */
+
     for(var y = 0; y * image2.height< canvas2.height;y++ ){
         dy = blockHeight * y;
         for(var x = 0; x < 13;x++){
