4731cf8ac5afa9999a030ef1dc7458d8088e1763
NakasatoH <nakasatoe50m@gmail.com>
2017-01-19 12:59:18 +0900

    メソッドの移動

--- static/js/ddPlusMove.js
+++ static/js/ddPlusMove.js
@@ -537,46 +537,6 @@ window.onload = function () {
     resetImage();
 }();
 
-function resetImage() {
-    var upper_elm = document.getElementById("upper");
-    var bottom_elm = document.getElementById("bottom");
-    // 右クリック時に画像をupperに戻し、配列からターゲットを排除する
-    bottom_elm.addEventListener("contextmenu", function (e) {
-        // メニュ表示をしない
-        e.preventDefault();
-        var target = e.target;
-        console.log(target.localName);
-        if (target.localName !== ("img") && target.className != ("divCode")) {
-            return;
-        }
-        console.log("wCnt: " + wCnt + " hMax : " + hMax);
-        outputArray2(target.id);
-        target.style.marginLeft = 0 + "px";
-        target.style.paddingRight = 4 + "px";
-        upper_elm.appendChild(target);
-    }, false);
-}
-
-/**
- * while画像をクリックした際、繰り返し回数と画像を変更する処理
- */
-function whileImageOnClick(e) {
-    switch (e.getAttribute("data-n")) {
-        case "2":
-            e.setAttribute("data-n", 3);
-            e.src = whileStr3Src;
-            break;
-        case "3":
-            e.setAttribute("data-n", 4);
-            e.src = whileStr4Src;
-            break;
-        case "4":
-            e.setAttribute("data-n", 2);
-            e.src = whileStr2Src;
-            break;
-    }
-}
-
 function outputArray2(id) {
     var e_index = 0;
     for (var i = 0; i < images.length; i++) {
